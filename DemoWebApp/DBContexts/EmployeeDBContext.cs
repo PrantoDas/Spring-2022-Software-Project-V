c394bb72f8290bba6509214173fe4d17905712fb
PRANTOPC\daspr <daspranto82@gmail.com>
2022-01-29 23:39:14 +0600

    Initial Implementation

---
+++ DemoWebApp/DBContexts/EmployeeDBContext.cs
@@ -0,0 +1,16 @@
+﻿using DemoWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoWebApp.DBContexts
+{
+    public class EmployeeDBContext : DbContext
+    {
+        public EmployeeDBContext(DbContextOptions<EmployeeDBContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ DemoWebApp/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace DemoWebApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+    }
+}
